14ffc38c33445b417c7e3d7aa12d198d01133a3d
djhart <dalton-hart@uiowa.edu>
2016-01-25 22:36:52 -0600

    node class + rudimentary builder

--- binary_search.rb
+++ binary_search.rb
@@ -1,9 +1,10 @@
 class Node
-	attr_accessor :parent, :lchild, :rchild
+	attr_accessor :parent, :lchild, :rchild, :value
 
 	def initialize(value, parent)
 		@value = value
 		@parent = parent
+		@lchild, @rchild = "none"
 	end
 
 end
@@ -14,6 +15,23 @@ def build_tree(arr)
 	center = Node.new(shuffed[0], "none")
 	tree = [] 
 	tree << center
-
-
+	shuffed.each do |x|
+		if x < tree[0].value
+			if tree[0].lchild == "none"
+				tree[0].lchild = Node.new(x, tree[0])
+				tree << tree[0].lchild
+			end
+		else
+			if tree[0].rchild == "none"
+				tree[0].rchild = Node.new(x, tree[0])
+				tree <<tree[0].rchild
+			end
+		end
+	end
+	return tree
 end
+
+arr = [1,2,3]
+
+tree = build_tree(arr)
+print tree
