66a53ee31eb7c1e073eaf17c252464c8e30a3926
djhart <dalton-hart@uiowa.edu>
2016-01-26 20:16:20 -0600

    insert and include? both working

--- binary_search.rb
+++ binary_search.rb
@@ -1,50 +1,70 @@
 class Node
-	attr_accessor :parent, :lchild, :rchild, :value, :count
+	attr_accessor :left, :right, :value, :count
 
-	def initialize(value, parent)
+	def initialize(value)
 		@value = value
-		@parent = parent
-		@lchild, @rchild = "none"
 		@count = 1
 	end
 
+	def insert(v)
+		case value <=> v
+		when 1 then insert_left(v)
+		when -1 then insert_right(v)
+		when 0 then self.count += 1
+		end
+	end
+
+	def inspect
+      "{#{value}::#{left.inspect}|#{right.inspect}}"
+    end
+
+    def insert_left(v)
+    	if left
+    		left.insert(v)
+    	else
+    		self.left = Node.new(v)
+    	end
+    end
+
+    def insert_right(v)
+    	if right 
+    		right.insert(v)
+    	else
+    		self.right = Node.new(v)
+    	end
+    end
+
+    def include?(v)
+   		case value <=> v
+    	when 1 then left.nil? ? false : left.include?(v)
+    	when -1 then right.nil? ? false : right.include?(v)
+    	when 0 then true
+    	end
+    end
+
 end
 
-def descend(x, node, tree)
-	if x < node.value
-		if node.lchild == "none"
-			node.lchild = Node.new(x, node)
-			tree << node.lchild
-		else 
-			y = node.lchild
-			puts y
-			descend(x, y, tree)
-		end
-	elsif x > node.value
-		if node.rchild == "none"
-			node.rchild = Node.new(x, node)
-			tree << node.rchild
-		else 
-			y = node.rchild
-			puts y
-			descend(x, y, tree)
-		end
-	else
-		node.count += 1
-	end
-end
 
 def build_tree(arr)
 	shuffed = arr.shuffle 
-	center = Node.new(shuffed[0], "none")
+	center = Node.new(shuffed[0])
 	tree = [] 
 	tree << center
-	shuffed[1..-1].each {|x| descend(x, tree[0], tree)}
+	shuffed[1..-1].each {|x| tree[0].insert(x)}
 	return tree
 end
 
-arr = [1,2,3]
+arr = [1,2,3,4,3,4,5,5,45,35,3,2,34,25,6,3,45,34,63,45,3,67,7,4,5,674,74,5,64,567,47,4,56,456,45,64,56,5]
 
 tree = build_tree(arr)
-#tree.each {|tree| print tree; puts tree.count}
-puts 
+bst = tree[0]
+
+
+puts bst.include?(15)
+puts bst.include?(1)
+puts bst.include?(13)
+puts bst.include?(5)
+puts bst.include?(45)
+puts bst.include?(100)
+puts bst.include?(4)
+puts bst.include?(3)
